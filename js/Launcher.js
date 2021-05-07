38e6fecdba55324d853531db629376198ad41d63
avanish44 <78263019+avanish44@users.noreply.github.com>
2021-05-07 04:56:37 -0700

    Add files via upload

---
+++ js/Launcher.js
@@ -0,0 +1,31 @@
+class Launcher{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.004,
+            length: 1
+        }
+        this.chain = Constraint.create(options);
+        World.add(world, this.chain);
+    }
+
+    display(){
+        if(this.chain.bodyA!=null){
+            var pointA = this.chain.bodyA.position;
+            var pointB = this.chain.pointB;
+            strokeWeight(4);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+        
+    }
+fly(){
+this.chain.bodyA = null;
+}
+attach(bodyA){
+this.chain.bodyA = bodyA;
+}
+
+       
+
+}
